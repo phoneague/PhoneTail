86561556ee8c96e082341996d5f2cf768916792e
HaramSong <163955252+Haramamama@users.noreply.github.com>
2024-05-29 17:48:26 +0900

    라미커밋ㅅ
    커밋완료

--- src/main/java/com/miniproject/phonetail/controller/customer/QnaListAction.java
+++ src/main/java/com/miniproject/phonetail/controller/customer/QnaListAction.java
@@ -7,6 +7,7 @@ import com.miniproject.phonetail.DAO.QuestionDAO;
 import com.miniproject.phonetail.DTO.MemberDTO;
 import com.miniproject.phonetail.DTO.QuestionDTO;
 import com.miniproject.phonetail.controller.action.Action;
+import com.miniproject.phonetail.util.Paging;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,20 +31,35 @@ public class QnaListAction implements Action {
 	        } else if (session.getAttribute("page") != null) { // 받은 페이지가 있으면
 	            page = (Integer) session.getAttribute("page"); // 바로 세션에다가 투입
 	        }
-
+	        
+	        String key="";
+			if(request.getParameter("key")!=null) {
+				key=request.getParameter("key");
+				session.setAttribute("key",key);
+			}else if(session.getAttribute("key") !=null) {
+				key = (String)session.getAttribute("key");
+			}else {
+				session.removeAttribute("key");
+			}
+			Paging paging = new Paging();
+			paging.setPage(page);
+			
+			QuestionDAO qdao = QuestionDAO.getInstance();
+			int count = qdao.getAllCount("question","userid",key);
+			paging.setTotalCount(count);
+			
 	        // 질문 목록을 가져오는 코드 추가
 	        ArrayList<QuestionDTO> questionList = new ArrayList<>();
-	        QuestionDAO qdao = QuestionDAO.getInstance();
-	        questionList = qdao.getAllQuestions(); // 모든 질문을 가져옵니다. 이에 따른 메서드명은 적절히 변경해주세요.
+	        questionList = qdao.getAllQuestions(paging,key); // 모든 질문을 가져옵니다. 이에 따른 메서드명은 적절히 변경해주세요.
 
 	        // 가져온 질문 목록을 request에 attribute로 저장합니다.
+	        request.setAttribute("paging", paging);
 	        request.setAttribute("questionList", questionList);
 
 	        // JSP 파일로 forward 합니다.
 	        request.getRequestDispatcher("customer/qnaList.jsp").forward(request, response);
 	    }
-	    
 	}
-	}
+}
 
 
---
+++ src/main/webapp/customer/script/customer.js
@@ -0,0 +1,5 @@
+function go_search(command){
+	var url="phonetail.do?command="	+command+"&page=1"
+	document.frm.action = url;
+	document.frm.submit();
+}
