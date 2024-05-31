4c543d6dc3194d5f9b5a1e9247d9aedfdf329352
doodadada <xodud6809@naver.com>
2024-05-31 18:04:08 +0900

    영이커밋
    영이커밋

--- src/main/java/com/miniproject/phonetail/DAO/ReportDAO.java
+++ src/main/java/com/miniproject/phonetail/DAO/ReportDAO.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.miniproject.phonetail.DTO.ReportDTO;
 import com.miniproject.phonetail.util.DB;
+import com.miniproject.phonetail.util.Paging;
 
 public class ReportDAO {
 	private ReportDAO() {}
@@ -66,5 +68,56 @@ public class ReportDAO {
 		}catch (SQLException e) { e.printStackTrace();
 		} finally { DB.close(con, pstmt, rs);  }
 	}
+
+	public int getMyAllCount(String tablename, String myId) {
+		int count = 0;
+		con = DB.getConnection();
+		String sql = "SELECT COUNT(*) AS cnt FROM " + tablename + " WHERE userid LIKE ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, myId);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				count = rs.getInt("cnt");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DB.close(con, pstmt, rs);
+		}
+		return count;
+	}
+
+	public ArrayList<ReportDTO> myReportList(Paging paging, String myId) {
+		ArrayList<ReportDTO>list = new ArrayList<>();
+		con = DB.getConnection();
+		String sql = "SELECT * FROM report WHERE userid LIKE ? "
+				+ " ORDER BY restate DESC, reseq DESC LIMIT ? OFFSET ?";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, myId);
+			pstmt.setInt(2, paging.getDisplayRow());
+			pstmt.setInt(3, paging.getStartNum() - 1);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				ReportDTO rdto = new ReportDTO();
+				rdto.setReseq(rs.getInt("reseq"));	
+				rdto.setPseq(rs.getInt("pseq"));	
+				rdto.setUserid(rs.getString("userid"));	
+				rdto.setRetype(rs.getInt("retype"));	
+				rdto.setRecontent(rs.getString("recontent"));	
+				rdto.setRestate(rs.getString("restate"));	
+				rdto.setIndate(rs.getTimestamp("indate"));	
+//				System.out.println(rdto.toString());
+				list.add(rdto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DB.close(con, pstmt, rs);
+		}
+		return list;
+	}
+
+	
 	
 }
