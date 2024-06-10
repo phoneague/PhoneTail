1cb78026ebf39a86829e08e98f32943acac07402
오빛나리 <solitarybiz@gmail.com>
2024-06-10 16:01:25 +0900

    나링커밋
    나링링~~

--- src/main/webapp/Database/phonetail.sql
+++ src/main/webapp/Database/phonetail.sql
@@ -24,6 +24,7 @@ DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS question;
 DROP TABLE IF EXISTS member;
 DROP VIEW IF EXISTS hak;
+DROP VIEW IF EXISTS hakk;
  
 -- 테이블 생성
 --CREATE TABLE address
